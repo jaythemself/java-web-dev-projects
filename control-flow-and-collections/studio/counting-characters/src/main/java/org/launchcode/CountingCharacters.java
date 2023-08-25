650804aaa3ad05ac6a62eed66fb24fceff241f0d
Jay <138824448+jaythemself@users.noreply.github.com>
2023-08-24 20:10:55 -0500

    adjusted for File class error

--- control-flow-and-collections/studio/counting-characters/src/main/java/org/launchcode/CountingCharacters.java
+++ control-flow-and-collections/studio/counting-characters/src/main/java/org/launchcode/CountingCharacters.java
@@ -1,11 +1,12 @@
 package org.launchcode;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Scanner;
 public class CountingCharacters
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws FileNotFoundException {
         //use Scanner here to get user input, close scanner after stored user input in new variable (input.close();)
         //other bonus directions: make character counts case-insensitive and exclude spaces/punctuation (validation)
         //System.out.println("Gandalf".toLowerCase().equals("gandalf")); prints true. if/else/else if identical to js
@@ -16,10 +17,10 @@ public class CountingCharacters
                 //if (nobleName.equals("")) {title = ""} else if etcetc else {System.out.println("Sorry, input is invalid.");}
                 //System.out.println(noble.get("keyname") + " is " + noble.get("keyvalue"));
 
-        //File file = new File ("C:\\Users\\void-\\Documents\\Git-Exercises\\intelliJ-Projects\\samplestring.txt");
+        File file = new File("C:\\Users\\void-\\Documents\\Git-Exercises\\intelliJ-Projects\\samplestring.txt");
         //if file works, Scanner changes to be Scanner input = new Scanner(file); and otherwise same as before
 
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(file);
         System.out.println("Please enter a sentence: ");
         String str = input.nextLine().toLowerCase(Locale.ROOT).replaceAll("[^a-zA-Z]", "");
         //String str = "Hello World!";
