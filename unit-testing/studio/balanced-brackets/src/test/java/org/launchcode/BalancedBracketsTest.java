3f54b8f733a38342e631959ca6e71df2e185cdb6
Jay <138824448+jaythemself@users.noreply.github.com>
2023-10-19 20:08:13 -0500

    added positive test cases (unit testing studio)

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -11,19 +11,53 @@ class BalancedBracketsTest {
     @Test
     public void onlyBracketsReturnsTrue() {
         String message = "a single set of balanced brackets returns true";
-        assertTrue(BalancedBrackets.hasBalancedBrackets("[]", message));
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"), message);
     }
 
     // TODO: Test balanced brackets in front of other characters -- "[]LaunchCode"
+    @Test
+    public void singleBracketsBeforeOtherCharacters() {
+        String message = "balanced brackets in front of other characters returns true";
+        String testData = "[]LaunchCode";
+        boolean result = BalancedBrackets.hasBalancedBrackets(testData);
+        assertTrue(result, message);
+    }
 
     // TODO: Test balanced brackets around other characters -- "[LaunchCode]"
+    @Test
+    public void singleBracketsAroundOtherCharacters() {
+        String message = "balanced brackets around other characters returns true";
+        String testData = "[LaunchCode]";
+        boolean result = BalancedBrackets.hasBalancedBrackets(testData);
+        assertTrue(result, message);
+    }
 
     // TODO: Test balanced brackets within other characters -- "Launch[Code]"
+    @Test
+    public void singleBracketsAmongOtherCharacters() {
+        String message = "balanced brackets among other characters returns true";
+        String testData = "Launch[Code}";
+        boolean result = BalancedBrackets.hasBalancedBrackets(testData);
+        assertTrue(result, message);
+    }
 
     // TODO: Test multiple consecutive pairs of balanced brackets -- "[][][]"
+    @Test
+    public void tripleBalancedBrackets() {
+        String message = "three consecutive pairs of balanced brackets returns true";
+        String testData = "[][][]";
+        boolean result = BalancedBrackets.hasBalancedBrackets(testData);
+        assertTrue(result, message);
+    }
 
     // TODO: Test multiple nested balanced brackets -- "[[[]]]"
-
+    @Test
+    public void tripleNestedBalancedBrackets() {
+        String message = "three sets of nested brackets returns true";
+        String testData = "[[[]]]";
+        boolean result = BalancedBrackets.hasBalancedBrackets(testData);
+        assertTrue(result, message);
+    }
 
 
     // NEGATIVE TEST CASES
